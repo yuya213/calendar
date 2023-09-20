590d0c2782a3c6dd0897e4213d72001497b5ecb9
yuya213 <129849154+yuya213@users.noreply.github.com@users.noreply.github.com>
2023-09-20 10:44:19 +0000

    Initial Commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,46 @@
+class BlogsController < ApplicationController
+  
+  def index
+    @blogs = Blog.all
+    @blog = Blog.new
+  end
+  
+  def new
+    @blog = Blog.new
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def create
+    Blog.create(blog_parameter)
+    redirect_to blogs_path
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path, notice:"削除しました"
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_parameter)
+      redirect_to blogs_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def blog_parameter
+    params.require(:blog).permit(:title, :content, :start_time)
+  end
+
+end
---
+++ db/migrate/20230920103701_create_blogs.rb
@@ -0,0 +1,12 @@
+class CreateBlogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blogs do |t|
+
+      t.string :title
+      t.text :content
+      t.datetime :start_time
+      
+      t.timestamps
+    end
+  end
+end
